6666fa1d7bdfe571a1472e559889a3ff3cc95428
Jonathan Shimwell <mail@jshimwell.com>
2022-02-25 14:52:05 +0000

    tests using local h5m file

--- tests/test_plot_slice_of_dagmc_file.py
+++ tests/test_plot_slice_of_dagmc_file.py
@@ -13,16 +13,7 @@ class TestPlotSliceOfDagmcFile(unittest.TestCase):
 
     def setUp(self):
 
-        if not Path("tests/v0.0.2.tar.gz").is_file():
-            url = "https://github.com/fusion-energy/neutronics_workflow/archive/refs/tags/v0.0.2.tar.gz"
-            urllib.request.urlretrieve(url, "tests/v0.0.2.tar.gz")
-
-            tar = tarfile.open("tests/v0.0.2.tar.gz", "r:gz")
-            tar.extractall("tests")
-            tar.close()
-
-        self.h5m_filename_smaller = "tests/neutronics_workflow-0.0.2/example_01_single_volume_cell_tally/stage_2_output/dagmc.h5m"
-        self.h5m_filename_bigger = "tests/neutronics_workflow-0.0.2/example_02_multi_volume_cell_tally/stage_2_output/dagmc.h5m"
+        self.h5m_filename_smaller = "tests/dagmc.h5m"
 
     def test_create_default_plot(self):
         """Tests returned object is a matplotlib plot"""
@@ -32,12 +23,6 @@ class TestPlotSliceOfDagmcFile(unittest.TestCase):
         )
 
         assert isinstance(plot, type(plt))
-
-        plot2 = plot_slice_of_dagmc_file(
-            dagmc_filename=self.h5m_filename_bigger,
-        )
-
-        assert isinstance(plot2, type(plt))
 
     def test_create_default_plot_file(self):
         """Tests output file creation"""
@@ -50,12 +35,3 @@ class TestPlotSliceOfDagmcFile(unittest.TestCase):
         )
 
         assert Path("test_plot.png").is_file()
-
-        os.system("rm test_plot2.png")
-
-        plot_slice_of_dagmc_file(
-            dagmc_filename=self.h5m_filename_bigger,
-            output_filename="test_plot2.png",
-        )
-
-        assert Path("test_plot2.png").is_file()
