9d7d85db42bb19d3c3c33ab87628355cf3feb1ed
shimwell <shimwell@users.noreply.github.com>
2022-12-02 16:08:49 +0000

    [skip ci] Apply formatting changes

--- examples/example_4_from_readme.py
+++ examples/example_4_from_readme.py
@@ -1,8 +1,8 @@
 from dagmc_geometry_slice_plotter import plot_slice
 
 plot = plot_slice(
-    dagmc_file_or_trimesh_object='dagmc.h5m',
-    plane_normal = [1, 0, 0],
+    dagmc_file_or_trimesh_object="dagmc.h5m",
+    plane_normal=[1, 0, 0],
     plane_origin=[10, 0, 0],
     rotate_plot=270,
 )
