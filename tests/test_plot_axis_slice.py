5abab4524a040070f7913c75010f504a710c1519
shimwell <shimwell@users.noreply.github.com>
2022-12-12 23:53:31 +0000

    [skip ci] Apply formatting changes

--- tests/test_plot_axis_slice.py
+++ tests/test_plot_axis_slice.py
@@ -24,7 +24,7 @@ class TestPlotSliceOfTrimeshObject(unittest.TestCase):
         """Tests returned object is a matplotlib plot"""
 
         plot = plot_axis_slice(
-            view_direction='x',
+            view_direction="x",
             dagmc_file_or_trimesh_object=self.trimesh_mesh_object_smaller,
         )
 
@@ -36,7 +36,7 @@ class TestPlotSliceOfTrimeshObject(unittest.TestCase):
         os.system("rm test_plot.png")
 
         plot = plot_axis_slice(
-            view_direction='y',
+            view_direction="y",
             dagmc_file_or_trimesh_object=self.trimesh_mesh_object_smaller,
         )
         plot.savefig("test_plot.png")
