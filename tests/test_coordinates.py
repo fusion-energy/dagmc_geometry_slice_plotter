09231217b23b4263ffab98a7f50192a907720252
shimwell <mail@jshimwell.com>
2023-02-10 22:32:47 +0000

    added tests for coords

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -3,7 +3,7 @@ import unittest
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-from dagmc_geometry_slice_plotter import plot_slice
+from dagmc_geometry_slice_plotter import get_slice_coordinates
 
 
 class TestPlotSliceOfDagmcFile(unittest.TestCase):
@@ -15,20 +15,27 @@ class TestPlotSliceOfDagmcFile(unittest.TestCase):
     def test_create_default_plot(self):
         """Tests returned object is a matplotlib plot"""
 
-        plot = plot_slice(
+        list_of_coords = get_slice_coordinates(
             dagmc_file_or_trimesh_object=self.h5m_filename_smaller,
         )
 
-        assert isinstance(plot, type(plt))
+        for xy in list_of_coords:
+            plt.plot(*xy, color="black", linewidth=1)
+        
+        assert isinstance(list_of_coords, list)
 
     def test_create_default_plot_file(self):
         """Tests output file creation"""
 
         os.system("rm test_plot.png")
 
-        plot = plot_slice(
+        list_of_coords = get_slice_coordinates(
             dagmc_file_or_trimesh_object=self.h5m_filename_smaller,
         )
-        plot.savefig("test_plot.png")
+        
+        for xy in list_of_coords:
+            plt.plot(*xy, color="black", linewidth=1)
 
-        assert Path("test_plot.png").is_file()
+        
+        assert isinstance(list_of_coords, list)
+        # TODO consider making the function return lists of floats not a TrackedArray
